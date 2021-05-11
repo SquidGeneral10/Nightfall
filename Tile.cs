d9586bbde4e916082507da933e30168e869716d4
SquidGeneral10 <78150458+SquidGeneral10@users.noreply.github.com>
2021-05-11 20:17:50 +0100

    Update Tile.cs

--- Tile.cs
+++ Tile.cs
@@ -15,13 +15,6 @@ namespace Nightfall
         Platform = 2, // Tiles that let the player stand on top of them, and also jump through the bottom of them to get to the top.
     }
 
-    enum DoorCollision // Controls tile collisions.
-    {
-        Exit = 0, // Doors that the player can use to complete a level.
-
-        Portal = 1, // Doors that the player can use to appear at another door, like a portal.
-    }
-
     struct Tile
     {
         public Texture2D Texture; // Controls how the tile will look.
@@ -30,29 +23,16 @@ namespace Nightfall
         public const int Width = 32; // All tiles have a hitbox 32 pixels wide.
         public const int Height = 32; // All tiles have a hitbox 32 pixels tall.
 
+        public const int DoorWidth = 64; // All door tiles will have a hitbox 64 pixels wide.
+        public const int DoorHeight = 72; // All door tiles will have a hitbox 72 pixels tall.
+
         public static readonly Vector2 Size = new Vector2(Width, Height);
+        public static readonly Vector2 DoorSize = new Vector2(DoorWidth, DoorHeight);
 
         public Tile(Texture2D texture, TileCollision collision)
         {
             Texture = texture;
             Collision = collision;
-        }
-    }
-
-    struct Door
-    {
-        public Texture2D Texture; // Controls how the door will look.
-        public DoorCollision Collision; // Controls the type of collision the door has.
-
-        public const int Width = 32; // All tiles have a hitbox 40 pixels wide.
-        public const int Height = 38; // All tiles have a hitbox 32 pixels tall.
-
-        public static readonly Vector2 Size = new Vector2(Width, Height);
-
-        public Door(Texture2D doortexture, DoorCollision doorcollision)
-        {
-            Texture = doortexture;
-            Collision = doorcollision;
         }
     }
 }
